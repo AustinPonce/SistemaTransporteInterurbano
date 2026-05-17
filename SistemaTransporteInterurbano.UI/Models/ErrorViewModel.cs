f24631debe0e4f9f5ed766cbaf19c0c66ded2f08
AustinPonce <167488463+AustinPonce@users.noreply.github.com>
2026-05-17 01:32:56 -0600

    Delete SistemaTransporteInterurbano.UI directory

--- SistemaTransporteInterurbano.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaTransporteInterurbano.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
