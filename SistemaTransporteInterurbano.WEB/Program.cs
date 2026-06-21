b2efbbc6718117a50e16cff1c1bfdc3bf313cacd
yurimdz <YURIDIA.MENDOZA@ucr.ac.cr>
2026-06-21 14:07:46 -0600

    Cambios en el API de viajes

--- SistemaTransporteInterurbano.WEB/Program.cs
+++ SistemaTransporteInterurbano.WEB/Program.cs
@@ -7,7 +7,11 @@ using SistemaTransporteInterurbano.WEB.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
